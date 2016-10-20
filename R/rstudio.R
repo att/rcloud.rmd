480c7c1d297fa2378b637756ffd8604d57cfdefc
Gábor Csárdi <csardi.gabor@gmail.com>
2016-10-20 11:39:05 +0100

    Addin: do the POST (& redirect), close gadget cleanly
    We use https://httpbin.org/post for now, for testing,
    until RCloud will be able to create a notebook via POST.

--- R/rstudio.R
+++ R/rstudio.R
@@ -2,6 +2,7 @@
 rstudio_to_rcloud_rmd <- function() {
 
   ui <- miniUI::miniPage(
+    add_resource_path(),
     miniUI::miniContentPanel(
       shiny::textInput("url", "RCloud URL"),
       actionButton("exportButton", "Export"),
@@ -12,6 +13,21 @@ rstudio_to_rcloud_rmd <- function() {
   server <- function(input, output, session) {
 
     shiny::observeEvent(input$exportButton, {
+
+      ## Get the text of the currently edited document
+      edit_ctx <- rstudioapi::getSourceEditorContext()
+      text <- paste(edit_ctx$contents, collapse = "\n")
+      filename <- file_path_sans_ext(basename(
+        edit_ctx$path %||% NA_character_
+      ))
+
+      session$sendCustomMessage(
+        type = "rcloudexport",
+        message = list(
+          text = text,
+          filename = filename
+        )
+      )
 
       shiny::stopApp()
     })
@@ -24,4 +40,15 @@ rstudio_to_rcloud_rmd <- function() {
   ##  viewer <- shiny::dialogViewer("Export to RCloud", 400, 300)
   viewer <- shiny::browserViewer()
   shiny::runGadget(ui, server, viewer = viewer)
+}
+
+add_resource_path <- function() {
+  shiny::addResourcePath(
+    "rcloudexport",
+    system.file("shiny", package = "rcloud.rmd")
+  )
+
+  shiny::tags$head(shiny::tags$script(
+    src = "rcloudexport/messagehandler.js"
+  ))
 }
---
+++ inst/shiny/messagehandler.js
@@ -0,0 +1,37 @@
+
+Shiny.addCustomMessageHandler(
+    "rcloudexport",
+    function(message) {
+        post_to_url(
+            'https://httpbin.org/post',
+            { 'rmd': JSON.stringify(message) }
+        );
+    }
+);
+
+// http://stackoverflow.com/questions/133925/
+// javascript-post-request-like-a-form-submit/3259946#3259946
+function post_to_url(path, params, method) {
+    method = method || "post";
+
+    var form = document.createElement("form");
+
+    //Move the submit function to another variable
+    //so that it doesn't get overwritten.
+    form._submit_function_ = form.submit;
+
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        var hiddenField = document.createElement("input");
+        hiddenField.setAttribute("type", "hidden");
+        hiddenField.setAttribute("name", key);
+        hiddenField.setAttribute("value", params[key]);
+
+        form.appendChild(hiddenField);
+    }
+
+    document.body.appendChild(form);
+    form._submit_function_(); //Call the renamed function.
+}
